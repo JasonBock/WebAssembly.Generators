1fa47748d8a8c47e798c2a1b13d99c5bbfd23287
Jason Bock <jason.r.bock@outlook.com>
2021-05-03 10:10:48 -0500

    FINALLY! Success :)

--- src/WebAssembly.Generators.NuGetHost/Program.cs
+++ src/WebAssembly.Generators.NuGetHost/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-//using WebAssembly.Runtime;
+using WebAssembly.Runtime;
 
-Console.Out.WriteLine("hi");
-
-//var collatz = Compile.FromBinary<CollatzTest>("collatz.wasm");
-//Console.Out.WriteLine(collatz(new ImportDictionary()).Exports.collatz(3));
+var collatz = Compile.FromBinary<CollatzTest>("collatz.wasm");
+Console.Out.WriteLine(collatz(new ImportDictionary()).Exports.collatz(3));
